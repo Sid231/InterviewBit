946d2c5e60276f19ca122b21fbbc5957ca230fc5
Kumar Siddhant <ksiddhant92@gmail.com>
2018-09-14 16:54:55 -0400

    Update Rearrange_Array.java

--- Rearrange_Array.java
+++ Rearrange_Array.java
@@ -10,13 +10,14 @@ Return : [0, 1]
 
 */
 
-void rearrange(int arr[], int n)  
-    { 
-        // First step: Increase all values by (arr[arr[i]]%n)*n 
-        for (int i = 0; i < n; i++) 
-            arr[i] += (arr[arr[i]] % n) * n; 
-  
-        // Second Step: Divide all values by n 
-        for (int i = 0; i < n; i++) 
-            arr[i] /= n; 
-    } 
+public class Solution {
+    public void arrange(ArrayList<Integer> a) {
+        int n = a.size();
+        for(int i=0; i<n; i++){
+            a.set(i,a.get(i)+((a.get(a.get(i)))%n)*n);
+        }
+        for(int i=0; i<n; i++){
+            a.set(i,a.get(i)/n);
+        }
+    }
+}
