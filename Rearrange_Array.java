2d38a3d5ec94f863cf9a5cb98a8e24f6fba48406
Kumar Siddhant <ksiddhant92@gmail.com>
2018-09-14 16:41:57 -0400

    Update Rearrange_Array.java

--- Rearrange_Array.java
+++ Rearrange_Array.java
@@ -1,3 +1,15 @@
+/*
+Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
+
+Lets say N = size of the array. Then, following holds true :
+All elements in the array are in the range [0, N-1]
+N * N does not overflow for a signed integer
+
+Input : [1, 0]
+Return : [0, 1]
+
+*/
+
 void rearrange(int arr[], int n)  
     { 
         // First step: Increase all values by (arr[arr[i]]%n)*n 
